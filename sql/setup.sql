a36316812d623c1a404aa74c7f9fe919adc97c36
Michael Rider <mikepdxrider@gmail.com>
2022-01-25 13:09:42 -0800

    starts project

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLES IF EXISTS ast_danger;
+DROP TABLES IF EXISTS phonelist;
 
-CREATE TABLE ast_danger (
+CREATE TABLE phonelist (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    user TEXT NOT NULL,
     phone_num INT NOT NULL
 )
